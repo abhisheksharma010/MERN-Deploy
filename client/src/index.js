dc0b97d1b2d3ddc860524cd67b4e9550d2c18f27
Abhishek Sharma <abhisheksharma32344@gmail.com>
2024-05-23 06:51:03 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from "axios";
+
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from "./context/auth";
 import { SearchProvider } from "./context/search";
 import { CartProvider } from "./context/cart";
 import "antd/dist/reset.css";
+axios.defaults.baseURL = "https://serverr-1.onrender.com"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
