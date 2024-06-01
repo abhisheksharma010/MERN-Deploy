2f46b4aec83640c49c03f35a6c9a7601694bb6ab
Abhishek Sharma <abhisheksharma32344@gmail.com>
2024-06-01 22:32:45 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   client/src/pages/Auth/Register.js
    	modified:   client/src/styles/AuthStyle.css
    	new file:   client/src/styles/Login.css
    	new file:   client/src/styles/Register.css

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,32 +1,44 @@
-import React, { useState } from 'react'
-import Layout from '../../componenets/layout/Layout'
+import React, { useState } from 'react';
+import Layout from '../../componenets/layout/Layout';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import "../../styles/AuthStyle.css";
+import "../../styles/Register.css"; // Make sure this path is correct
+
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const [answer, setAnswer] = useState("");
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    password: "",
+    confirmPassword: "",
+    answer: ""
+  });
+
   const navigate = useNavigate();
 
-  // form function
+  const handleNextStep = () => {
+    setStep(step + 1);
+  };
+
+  const handlePreviousStep = () => {
+    setStep(step - 1);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
-    // console.log(`${process.env.REACT_APP_API}`);
     e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
     try {
-      const res = await axios.post(`/api/v1/auth/register`, {
-        name,
-        email,
-        password,
-        phone,
-        address,
-        answer,
-      });
+      const res = await axios.post(`/api/v1/auth/register`, formData);
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
         navigate("/login");
@@ -40,80 +52,138 @@ const Register = () => {
   };
 
   return (
-    <Layout title="Register - Ecommer App">
-      <div className="form-container" style={{ minHeight: "90vh" }}>
-        <form onSubmit={handleSubmit}>
-          <h4 className="title">REGISTER FORM</h4>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Name"
-              required
-              autoFocus
-            />
+    <Layout title="Register - Ecommerce App">
+      <h1 className="register-form-title">Register Form</h1>
+      <div className="register-form-container">
+        <div className="register-form-image">
+          <img src="https://img.freepik.com/premium-photo/online-shopping-concept-web-mobile-application-ecommerce-smartphone-with-shopping-cart-shopping-bag-yellow-background-3d-rendering_20693-1081.jpg" alt="Registration" />
+        </div>
+        <form onSubmit={handleSubmit} className="register-form">
+
+          <div>
+            <div className="register-steps">
+              <hr className='line-connect' />
+              {[1, 2, 3].map((num) => (
+                <div
+                  key={num}
+                  className={`register-step ${step >= num ? 'active' : ''}`}
+                  onClick={() => setStep(num)}
+                >
+                  {num}
+                </div>
+              ))}
+            </div>
+
           </div>
-          <div className="mb-3">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Email "
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter Your Password"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Phone"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Address"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={answer}
-              onChange={(e) => setAnswer(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="What is Your Favorite sports"
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            REGISTER
-          </button>
+          {step === 1 && (
+            <div className="register-form-step">
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="form-control"
+                  placeholder="Enter Your Name"
+                  required
+                  autoFocus
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="form-control"
+                  placeholder="Enter Your Email"
+                  required
+                />
+              </div>
+              <button type="button" className="btn btn-primary" onClick={handleNextStep}>
+                Next
+              </button>
+            </div>
+          )}
+          {step === 2 && (
+            <div className="register-form-step">
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  className="form-control"
+                  placeholder="Enter Your Phone"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleChange}
+                  className="form-control"
+                  placeholder="Enter Your Address"
+                  required
+                />
+              </div>
+              <div className="register-button">
+                <button type="button" className="btn btn-secondary" onClick={handlePreviousStep}>
+                  Previous
+                </button>
+                <button type="button" className="btn btn-primary" onClick={handleNextStep}>
+                  Next
+                </button>
+              </div>
+            </div>
+          )}
+          {step === 3 && (
+            <div className="register-form-step">
+              <div className="form-group">
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="form-control"
+                  placeholder="Enter Your Password"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  className="form-control"
+                  placeholder="Confirm Your Password"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="answer"
+                  value={formData.answer}
+                  onChange={handleChange}
+                  className="form-control"
+                  placeholder="What is Your Favorite Sport"
+                  required
+                />
+              </div>
+              <div className="register-button">
+                <button type="button" className="btn btn-secondary" onClick={handlePreviousStep}>
+                  Previous
+                </button>
+                <button type="submit" className="btn btn-success">
+                  Register
+                </button>
+              </div>
+            </div>
+          )}
         </form>
       </div>
     </Layout>
