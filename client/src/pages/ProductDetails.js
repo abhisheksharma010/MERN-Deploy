355b1d2f77271a9f177fd821edb882a91dd86de9
Abhishek Sharma <abhisheksharma32344@gmail.com>
2024-06-04 19:56:03 +0530

    modified:   client/src/pages/ProductDetails.js 	modified:   client/src/pages/Purchase.js 	modified:   client/src/styles/Purchase.css 	modified:   client/src/styles/Rating.css

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -2,22 +2,20 @@ import Layout from '../componenets/layout/Layout';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useCart } from '../context/cart';
+import { useAuth } from '../context/auth'; // Import useAuth
 import { CiHeart } from "react-icons/ci";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaRegStar, FaStar } from "react-icons/fa";
 import { useParams, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import ProductCard from '../componenets/ProductCard';
-
-import { FaRegStar } from "react-icons/fa";
-import { FaStar } from "react-icons/fa";
 import "../styles/Rating.css"
-
 
 const ProductDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
 
   const [cart, setCart] = useCart();
+  const [auth, setAuth] = useAuth(); // Use auth context
   const [ratings, setRatings] = useState([]);
   const [averageRating, setAverageRating] = useState(null);
   const [reviews, setReviews] = useState([]);
@@ -25,7 +23,6 @@ const ProductDetails = () => {
   const [relatedProducts, setRelatedProducts] = useState([]);
   const [rating, setRating] = useState(null);
   const [message, setMessage] = useState("");
-
 
   const handleClick = (value) => {
     setRating(value === rating ? null : value);
@@ -40,90 +37,71 @@ const ProductDetails = () => {
       });
 
       console.log(response.data);
-      toast.success("Rating added to the cart");
+      toast.success("Rating added successfully");
     } catch (error) {
-      toast.error("Rating not added to the cart");
+      toast.error("Rating not added");
     }
     setRating(null);
     setMessage("");
-
-
   };
 
-  //initalp details
   useEffect(() => {
     if (params?.slug) {
       getProduct();
     }
   }, [params?.slug]);
 
-  //getProduct
   const getProduct = async () => {
     try {
-      const { data } = await axios.get(
-        `/api/v1/product/get-product/${params.slug}`
-      );
+      const { data } = await axios.get(`/api/v1/product/get-product/${params.slug}`);
       setProduct(data?.product);
       getSimilarProduct(data?.product?._id, data?.product.category._id);
-
       fetchProductRating();
     } catch (error) {
       console.log(error);
     }
   };
-  //get similar product
+
   const getSimilarProduct = async (pid, cid) => {
     try {
-      const { data } = await axios.get(
-        `/api/v1/product/related-product/${pid}/${cid}`
-      );
+      const { data } = await axios.get(`/api/v1/product/related-product/${pid}/${cid}`);
       setRelatedProducts(data?.products);
-      // fetchProductRating();
     } catch (error) {
       console.log(error);
     }
   };
+
   const fetchProductRating = async () => {
     const productId = product?._id;
-    console.log("reached");
-
     try {
-      // console.log("ese");
       const response = await axios.get(`/api/v1/product/get-product-rating`, { params: { productId } });
-      // console.log(response.data);
-
       const { averageRating, messages } = response.data;
       setAverageRating(averageRating);
       setReviews(messages);
-      console.log(averageRating);
-      console.log(reviews);
     } catch (error) {
-      // console.log("eses");
       console.error("Error fetching product rating:", error);
     }
-    console.log("clicked");
   };
+
   return (
     <Layout>
-      <div className="row container product-details-container">
-        <div className="col-md-4 product-image-container" style={{ marginBottom: '1.5rem' }}> {/* Updated class names and added inline style */}
+      <div className="product-details-container container">
+        <div className="product-image-container">
           <img
             src={`https://yoy.onrender.com/api/v1/product/product-photo/${product?._id}`}
             alt={product.name}
-            height="200"
-            width={"200px"}
-            className='asd'
+            className="product-image"
           />
         </div>
-        <div className="col-md-6 product-info-container">
+        <div className="product-info-container">
           <h1 className="text-center">Product Details</h1>
           <h6>Name : {product.name}</h6>
           <h6>Description : {product.description}</h6>
-          <h6>Price : {product.price}</h6>
+          <h6>Price : &#8377;{product.price * 100}</h6>
           <h6>Category : {product?.category?.name}</h6>
           {cart.includes(product._id) ? (
             <FaHeart
-              style={{ fontSize: "22px", color: "red" }}
+              style={{ fontSize: "28px", color: "red" }}
               onClick={() => {
                 setCart(cart.filter(itemId => itemId !== product._id));
                 toast.success("Item removed from the cart");
@@ -131,7 +109,7 @@ const ProductDetails = () => {
             />
           ) : (
             <CiHeart
-              style={{ fontSize: "22px", color: "red" }}
+              style={{ fontSize: "28px", color: "red" }}
               onClick={() => {
                 setCart([...cart, product._id]);
                 toast.success("Item added to the cart");
@@ -139,7 +117,7 @@ const ProductDetails = () => {
             />
           )}
           <button
-            className="btn btn-success ms-1"
+            className="btn btn-success ms-2 buy-button"
             onClick={() => navigate(`/purchase/${product.slug}`)}
           >
             Buy
@@ -147,40 +125,39 @@ const ProductDetails = () => {
         </div>
       </div>
 
-
       <hr />
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col-md-12 mb-3">
-            {[1, 2, 3, 4, 5].map((starValue) => (
-              <span
-                key={starValue}
-                onClick={() => handleClick(starValue)}
-                style={{ cursor: "pointer", fontSize: "24px", marginRight: "0.5rem" }}
-              >
-                {rating && starValue <= rating ? <FaStar color="#ffc107" /> : <FaRegStar color="#ffc107" />}
-              </span>
-            ))}
-          </div>
-          <div className="col-md-12 mb-3">
-            <textarea
-              className="form-control"
-              placeholder="Enter your message..."
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </div>
-          <div className="col-md-12">
-            <button className="btn btn-primary" onClick={handleSubmit}>
-              Submit Rating
-            </button>
+      {auth?.user && (
+        <div className="container mt-4">
+          <div className="row">
+            <div className="col-md-12 mb-3">
+              {[1, 2, 3, 4, 5].map((starValue) => (
+                <span
+                  key={starValue}
+                  onClick={() => handleClick(starValue)}
+                  style={{ cursor: "pointer", fontSize: "24px", marginRight: "0.5rem" }}
+                >
+                  {rating && starValue <= rating ? <FaStar color="#ffc107" /> : <FaRegStar color="#ffc107" />}
+                </span>
+              ))}
+            </div>
+            <div className="col-md-12 mb-3">
+              <textarea
+                className="form-control"
+                placeholder="Enter your message..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </div>
+            <div className="col-md-12">
+              <button className="btn btn-primary" onClick={handleSubmit}>
+                Submit Rating
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
 
-
-
-      <div className="row container ">
+      <div className="row container">
         <h6>Similar Products</h6>
         {relatedProducts.length < 1 && (
           <p className="text-center">No Similar Products found</p>
@@ -188,7 +165,6 @@ const ProductDetails = () => {
         <div className="d-flex flex-wrap">
           {relatedProducts?.map((p) => (
             <ProductCard key={p._id} product={p} />
-
           ))}
         </div>
         {reviews.length > 0 && (
@@ -210,18 +186,18 @@ const ProductDetails = () => {
         )}
       </div>
       <button onClick={() => { fetchProductRating(); }} className='btn btn-success ml-5 m-5'> Show Product Rating</button>
-
     </Layout>
   );
 };
+
 const renderStars = (rating) => {
   const stars = [];
   for (let i = 1; i <= 5; i++) {
-    if (i <= rating) {
-      stars.push(<FaStar color="#ffc107" />);
-    } else {
-      stars.push(<FaRegStar color="#ffc107" />);
-    }
+    stars.push(
+      i <= rating
+        ? <FaStar key={i} color="#ffc107" />
+        : <FaRegStar key={i} color="#ffc107" />
+    );
   }
   return stars;
 }
